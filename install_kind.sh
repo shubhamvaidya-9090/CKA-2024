a27cfb01a3318e4038520a0e1923f8b0ae9ea440
Shubham <53913476+shubhamvaidya-9090@users.noreply.github.com>
2024-08-04 14:25:57 +0530

    sh script

--- install_kind.sh
+++ install_kind.sh
@@ -4,15 +4,11 @@ sudo apt update
 sudo apt install docker.io -y
 sudo systemctl start docker
 sudo usermod -aG docker ubuntu
-
 newgrp docker
 docker run hello-world
-
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.23.0/kind-linux-amd64
-
 [ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.23.0/kind-linux-arm64
-
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
