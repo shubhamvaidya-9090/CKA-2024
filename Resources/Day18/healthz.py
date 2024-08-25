182159a8f16af52fbe60a0cccf3ef4c1cf46717c
Shubham <53913476+shubhamvaidya-9090@users.noreply.github.com>
2024-08-25 13:29:47 +0530

    Change

---
+++ Resources/Day18/healthz.py
@@ -0,0 +1,15 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class HealthCheckHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/tmp/healthz':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'OK')
+        else:
+            self.send_error(404)
+
+httpd = HTTPServer(('', 8080), HealthCheckHandler)
+print("Server running on port 8080")
+httpd.serve_forever()
