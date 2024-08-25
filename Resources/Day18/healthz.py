e951fc0560242628df0c7504124ac955787c13a4
Shubham <53913476+shubhamvaidya-9090@users.noreply.github.com>
2024-08-25 13:44:58 +0530

    Change

--- Resources/Day18/healthz.py
+++ Resources/Day18/healthz.py
@@ -2,7 +2,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class HealthCheckHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/tmp/healthz':
+        if self.path == '/':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
